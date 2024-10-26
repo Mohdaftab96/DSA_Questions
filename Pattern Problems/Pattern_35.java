e6e9f3a5b22fda3d2675b6a7e51357e9f2b079ac
Mohd Aftab <93048917+Mohdaftab96@users.noreply.github.com>
2024-10-26 10:08:41 +0530

    Add files via upload

---
+++ Pattern Problems/Pattern_35.java
@@ -0,0 +1,56 @@
+
+
+// Enter the value : 5
+// 2 4 6 8 10 
+// 12 14 16 18 20
+// 22 24 26 28 30 
+// 32 34 36 38 40
+// 42 44 46 48 50
+
+
+
+import java.util.Scanner;
+public class Pattern_35{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value : ");
+        int n = sc.nextInt(); 
+        
+       
+       int a = 2;
+        int count = 1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                    System.out.print(a * count + " ");
+                    count = count + 1;
+            }
+            System.out.println();
+        }
+
+       
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
