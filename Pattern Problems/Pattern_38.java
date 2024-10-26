c59ec389837494a465bb3e1173618bce5dec05aa
Mohd Aftab <93048917+Mohdaftab96@users.noreply.github.com>
2024-10-26 10:55:53 +0530

    Update Pattern_38.java

--- Pattern Problems/Pattern_38.java
+++ Pattern Problems/Pattern_38.java
@@ -36,7 +36,38 @@ public class Pattern_38{
     }
 }
 
+//===================Problem - 02=================
 
+// Enter the value : 5
+// 5 10 15 20 25 
+// 4 9 14 19 24 
+// 3 8 13 18 23 
+// 2 7 12 17 22
+// 1 6 11 16 21 
+
+
+import java.util.Scanner;
+public class Pattern_38_GitHub{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value : ");
+        int n = sc.nextInt(); 
+        
+       
+        for(int i=1; i<=n; i++){
+            int count = n-i+1;
+            for(int j=1; j<=n; j++){
+                
+                System.out.print(count + " ");
+                count = count + 5;
+            }
+            System.out.println();
+        }
+
+
+    }
+}
 
 
 
