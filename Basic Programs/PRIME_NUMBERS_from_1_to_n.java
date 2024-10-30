4b450baa8bd58c893bf4ab8cd902b2b60001c5b4
Mohd Aftab <93048917+Mohdaftab96@users.noreply.github.com>
2024-10-30 11:07:15 +0530

    Update PRIME_NUMBERS_from_1_to_n.java

--- Basic Programs/PRIME_NUMBERS_from_1_to_n.java
+++ Basic Programs/PRIME_NUMBERS_from_1_to_n.java
@@ -1,3 +1,48 @@
+
+// Enter the Value of s1 : 20
+// Enter the Value of s2 : 40
+// 23 29 31 37 
+
+// Enter the Value of s1 : 1
+// Enter the Value of s2 : 20
+// 1 2 3 5 7 11 13 17 19 
+
+// Enter the Value of s1 : 1
+// Enter the Value of s2 : 40
+// 1 2 3 5 7 11 13 17 19 23 29 31 37 
+
+
+import java.util.Scanner;
 public class PRIME_NUMBERS_from_1_to_n {
-      
+      public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter the Value of s1 : ");
+            int s1 = sc.nextInt(); // s1 = 1
+
+            System.out.print("Enter the Value of s2 : ");
+            int s2 = sc.nextInt(); // s2 = 20
+
+
+            int flag = 1;
+            int i;
+            for(i=s1; i<=s2; i++){
+                  for(int j=2; j<i; j++){
+                        if(i%j == 0){
+                              flag = 0;
+                              break;
+                        }else{
+                              flag = 1;
+                        }
+                  }
+
+                  if(flag == 1){
+                        System.out.print(i + " ");
+                  }
+            }
+
+           
+
+                  
+      }
 }
