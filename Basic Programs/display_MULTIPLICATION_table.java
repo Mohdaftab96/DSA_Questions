688ed4afe0b5a60ec285f5cdb5237bb384eb63bd
Mohd Aftab <93048917+Mohdaftab96@users.noreply.github.com>
2024-10-30 11:40:06 +0530

    Add files via upload

---
+++ Basic Programs/display_MULTIPLICATION_table.java
@@ -0,0 +1,30 @@
+
+// Enter value of n : 5
+// 5*1 = 5
+// 5*2 = 10
+// 5*3 = 15
+// 5*4 = 20
+// 5*5 = 25
+// 5*6 = 30
+// 5*7 = 35
+// 5*8 = 40
+// 5*9 = 45
+// 5*10 = 50
+
+
+import java.util.Scanner;
+public class display_MULTIPLICATION_table {
+      public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter value of n : ");
+            int n = sc.nextInt();
+
+            for(int i=1; i<=10; i++){
+                  System.out.println(n + "*" + i + " = " + n*i);
+            }
+
+
+
+      }
+}
